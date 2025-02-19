63c8a98b6cc50d781631f00ebcd27ae1b1632b36
shailendr pawar <shailendrapawar980@gmail.com>
2025-02-19 20:38:19 +0530

    ADDED: structure of left side home page

---
+++ src/components/otheUserList/OtherUserList.jsx
@@ -0,0 +1,8 @@
+const OtherUserList = () => {
+  return (
+    <div className=" min-h-15">
+        OtherUserList
+    </div>
+  )
+}
+export default OtherUserList
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,11 +1,13 @@
+import "./home.css"
 import { useEffect } from "react"
 import {useSelector} from "react-redux"
 import {  useNavigate } from "react-router-dom"
 
 import { IoColorFilterOutline } from "react-icons/io5";
-
+import OtherUserList from "../../components/otheUserList/OtherUserList"
 const Home = () => {
   const navigate=useNavigate()
+  const tempArr=[1,2,3,4,5,6]
   const user=useSelector(state=>state.user)
   useEffect(()=>{
     if(!user.authUser){
@@ -19,7 +21,7 @@ const Home = () => {
   return (
     <main className="w-[90%] max-w-[800px] h-[500px] bg-green-300 flex">
       
-      <section className="w-2/6">
+      <section className="w-2/6 relative">
         <div className="w-100% h-[20%] flex">
 
           <div className="bg-white h-full w-[60%] flex flex-col justify-between items-start ">
@@ -27,18 +29,25 @@ const Home = () => {
            <span>{"username"}</span>
           </div>
 
-          <section className="w-[40%] flex items-center">
+          <section className="w-[40%] flex items-center justify-center">
           <b>THEME</b>
           </section>
-
         </div>
 
-        <div className="h-[70%]">
-          online and othe user
+
+        <div className="h-[70%] flex flex-col justify-evenly">
+          <section className="h-10 flex justify-evenly items-center">
+            <input type="text" className="w-[75%] rounded-2xl h-7 text-sm pl-1 pr-1"/>
+            <button className="w-[20%] rounded-md text-xs h-7 ">search</button>
+          </section>
+
+          <section className="user-list h-[80%] overflow-y-scroll flex flex-col gap-2  p-2">
+            {tempArr.map((v,i)=>{
+              return  <OtherUserList/>
+            })}
+          </section>
         </div>
-
-        
-
+        <button className="absolute bottom-3 left-3 w-20 h-8 rounded-md bg-red-500 text-white">logout</button>
       </section>
 
 
